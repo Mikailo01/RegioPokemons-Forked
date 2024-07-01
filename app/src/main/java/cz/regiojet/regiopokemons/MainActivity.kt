f2c0699d6f2a59653d670bbf46a13443c94bd2cc
Jacob Derynk <jacob.derynk@regiojet.cz>
2024-07-01 17:25:04 +0200

    Add UI state data class to VM

--- app/src/main/java/cz/regiojet/regiopokemons/MainActivity.kt
+++ app/src/main/java/cz/regiojet/regiopokemons/MainActivity.kt
@@ -4,27 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import cz.regiojet.regiopokemons.ui.theme.RegioPokemonsTheme
-import org.koin.android.ext.android.inject
 import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
@@ -49,7 +34,10 @@ private fun MainActivityScreen(
 ) {
 
     // TODO 2.
-    //  - collect state, show loading state, button to fetch pokemon list
+    //  - show loading state, add button to fetch pokemon list
     //  - print pokemon list on the screen
+
+    // Todo uncomment to collect state from the VM
+    //val state = viewModel.uiState.collectAsState()
 
 }
